3fa3f0c7b6a682bce0c2d090a4064048b4c2b143
yuuwe <duy.yuuwe@pm.me>
2024-11-14 13:48:46 -0800

    lalala

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,34 @@
 #include <ECE3.h>
 
-/*
- * TODO: FIX P AND D TERMS
- * 
- * 
- */
-
 uint16_t sensor_values[8]; // sensor values: right -> left, 0 -> 7
 
-const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
+const int left_nslp_pin=31;
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
 
-const int right_nslp_pin=11; // nslp ==> awake & ready for PWM
+const int right_nslp_pin=11;
 const int right_dir_pin=30;
 const int right_pwm_pin=39;
 
+const int bump_0 = 24; // this bump is on the right
+const int bump_5 = 28; // lets use this bump because it is on the left side
+// TRUE => bump is not switched
+// FALSE => bump is switched
+
 const int LED_RF = 41;
 
+// pwm signal ranges from: 0 - 255 
+int max_speed = 127;
+int base_speed = 40;
+
+int l_speed = base_speed;
+int r_speed = base_speed;
+
+int prev_error = 0;
+
 int weights[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
+int mins[8] = {756, 595,686,503,572,663,641,706};
+int maxs[8] = {1744,1321,1658,964,1155,1718,1489,1794};
 
 void setup() {
   ECE3_Init();
@@ -38,40 +48,58 @@ void setup() {
   digitalWrite(right_dir_pin,LOW);
   digitalWrite(right_nslp_pin,HIGH);
 
+  resetEncoderCount_left();
+  resetEncoderCount_right();
+
   delay(4000); // wait for setup
 }
 
-// pwm signal ranges from: 0 - 255 
-
-int max_speed = 127;
-int base_speed = 40;
-
-int l_speed = base_speed;
-int r_speed = base_speed;
-
-
-float K_P = 0.01;
-float K_D = 0.005;
-
-int prev_error = 0;
-
-
-int mins[8] = {756, 595,686,503,572,663,641,706};
-int maxs[8] = {1744,1321,1658,964,1155,1718,1489,1794};
-
-/*
-int mins[8] = {706, 641, 663, 572, 503, 686, 595, 756};
-int maxs[8] = {1794, 1489, 1718, 1155, 964, 1658, 1321, 1744};
-*/
-
-void nlsp_low() {
-  digitalWrite(left_nslp_pin, LOW);
-  digitalWrite(right_nslp_pin, LOW);
+void set_nlsp(bool wheels) {
+  // true => turn on wheels
+  // false => turn off wheels
+  digitalWrite(left_nslp_pin, wheels);
+  digitalWrite(right_nslp_pin, wheels);
 }
 
-void test() { // this function will loop
+void set_forward() {
+  digitalWrite(left_dir_pin,LOW);
+  digitalWrite(right_dir_pin,LOW);
+}
+
+void set_right() {
+  digitalWrite(left_dir_pin,LOW);
+  digitalWrite(right_dir_pin,HIGH);
+}
+
+void set_left() {
+  digitalWrite(left_dir_pin,HIGH);
+  digitalWrite(right_dir_pin,LOW);
+}
+
+int avg_encoder() {
+  int l_encoder = getEncoderCount_left();
+  int r_encoder = getEncoderCount_right();
+  return ((l_encoder + r_encoder) / 2.0);
+}
+
+int revs() { // returns wheel revolutions
+  int revolutions = avg_encoder() / 360;
+  return revolutions;
+}
+
+void test_encoder() {
+  set_nlsp(true);
+  analogWrite(left_pwm_pin, 40);
+  analogWrite(right_pwm_pin, 40);
+  Serial.print(avg_encoder());
+  Serial.print('\t');
+  Serial.print(revs());
+  Serial.println();
+}
+
+void test(bool wheels = false) { // this function will loop
   
-  nlsp_low(); // TURN OFF WHEELS
+  set_nlsp(wheels); // TURN OFF WHEELS
   
   ECE3_read_IR(sensor_values);
   int error = calc_error(sensor_values);
@@ -98,26 +126,11 @@ void test() { // this function will loop
   delay(500);
 }
 
-void set_forward() {
-  digitalWrite(left_dir_pin,LOW);
-  digitalWrite(right_dir_pin,LOW);
-}
-
-void set_right() {
-  digitalWrite(left_dir_pin,LOW);
-  digitalWrite(right_dir_pin,HIGH);
-}
-
-void set_left() {
-  digitalWrite(left_dir_pin,HIGH);
-  digitalWrite(right_dir_pin,LOW);
-}
-
 int calc_error(uint16_t input[8]) { // takes input of sensor values
 
 /*
- * Positive error : black line is to the left => ERROR = -40mm => error > 0 : steer to the right
- * Negative error : black line is to the right => ERROR = 40mm => error < 0 : steer to the left
+ * Negative error : black line is to the left => ERROR = -40mm => error < 0 : steer to the left
+ * Positive error : black line is to the right => ERROR = 40mm => error > 0 : steer to the right
  */
   
   int error;
@@ -137,6 +150,9 @@ int calc_error(uint16_t input[8]) { // takes input of sensor values
   return error;
 }
 
+float K_P = 0.01;
+float K_D = 0.005;
+
 int calc_p(int error) {
   int p = K_P * abs(error);
   return p;
@@ -148,43 +164,63 @@ int calc_d(int error) {
 }
 
 int* control_car(int error) {
-   //*  error > 0 : track is to the left
-   //* error < 0 : track is to the right
+   //*  error > 0 : track is to the RIGHT
+   //* error < 0 : track is to the LEFT
 
-   int pwm[2];
+   static int pwm[2]; // [0] is left_pwm, [1] is right_pwm
 
    int p = calc_p(error);
 
-  if (error < 0) { // error < 0, Track is to the left, steer left !!! THIS WORKS, ADJUST K_P
-      l_speed = base_speed;                             // Keep left wheel at base speed
-      r_speed = base_speed + p; // Speed up right wheel to steer right                     
-   } else { // error > 0, Track is to the right, steer right
-      l_speed = base_speed + p; // Speed up right wheel to steer right
-      r_speed = base_speed;                             // Keep left wheel at base speed
-   }
+   if (error < 0) { // error < 0, Track is to the left, steer left
+      l_speed = base_speed + p; // Speed up left wheel
+      r_speed = base_speed - p; // Slow down right wheel                  
+    } else { // error > 0, Track is to the right, steer right
+      l_speed = base_speed - p; // Slow down left wheel
+      r_speed = base_speed + p; // Speed up right wheel
+    }
 
   //l_speed = constrain(l_speed, 0, max_speed);
-  // r_speed = constrain(r_speed, 0, max_speed);
+  //r_speed = constrain(r_speed, 0, max_speed);
+  
   analogWrite(left_pwm_pin, l_speed);
   analogWrite(right_pwm_pin, r_speed);
-
-  l_speed=0;
-  r_speed=0;
 
   pwm[0] = l_speed;
   pwm[1] = r_speed;
   return pwm;
-  
+}
+
+void start() {
+  set_forward();
+  ECE3_read_IR(sensor_values);
+  int error = calc_error(sensor_values);
+  control_car(error);
+}
+
+// TODO, do data collection
+
+int data[0][2];
+int store_data() {
+}
+
+void stop_car() {
+  analogWrite(left_pwm_pin, 0);
+  analogWrite(right_pwm_pin, 0);
+}
+
+void duration(int max_revs){ // stops after number of revolutions
+  if (revs() < max_revs){
+    stop_car();
+  }
 }
 
 void loop() {
-  set_forward(); // make sure direction of car is forward
 
-  test();
+  bool s = analogRead(bump_5);
+  Serial.println(s);
+  delay(500);
+  
+  //test(false);
+  //start();
 
-  //ECE3_read_IR(sensor_values);
-  //int error = calc_error(sensor_values);
-  
-  //control_car(error);
-  
   }
